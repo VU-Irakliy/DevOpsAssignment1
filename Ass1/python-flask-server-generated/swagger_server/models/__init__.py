6fd57e64fa152d6d75885b4532ca346614718262
VU-Irakliy <i.marsagishvili@student.vu.nl>
2024-02-14 12:33:20 +0100

    rearrange

--- Ass1/python-flask-server-generated/swagger_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.grade_record import GradeRecord
-from swagger_server.models.inline_response200 import InlineResponse200
-from swagger_server.models.student import Student
